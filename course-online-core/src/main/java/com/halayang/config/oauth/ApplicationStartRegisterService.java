86d3e214bf04e51859ab0c1e2cd107fdbb9dfd54
杨宇迪 <yangyudi@hatech.com.cn>
2021-05-13 09:28:40 +0800

    fix: 配置类优化

--- course-online-core/src/main/java/com/halayang/config/oauth/ApplicationStartRegisterService.java
+++ course-online-core/src/main/java/com/halayang/config/oauth/ApplicationStartRegisterService.java
@@ -7,18 +7,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-import org.springframework.util.Base64Utils;
 
 /**
  * copyright (C), 2019, 北京同创永益科技发展有限公司
  *
- * @program istorm-automation-core
- * 系统启动完成监听
  * @author WangMingShuai
- * @create 2019/12/20 15:27
  * @version 2.0.0
  * <author>                <time>                  <version>                   <description>
  * Yangyudi         2019/12/20 15:27             2.0.0                         系统启动完成监听
+ * @program istorm-automation-core
+ * 系统启动完成监听
+ * @create 2019/12/20 15:27
  */
 @Component
 @Slf4j
@@ -33,6 +32,8 @@ public class ApplicationStartRegisterService implements ApplicationRunner {
     private String clientSecret;
     @Value("${security.oauth2.client.resource-ids}")
     private String resourceIds;
+    @Value("${security.address}")
+    private String address;
 
     /**
      * 注册oauth service
@@ -45,21 +46,22 @@ public class ApplicationStartRegisterService implements ApplicationRunner {
 
     /**
      * 项目启动后将服务注册到 oauth-server
+     *
      * @param args ApplicationArguments对象
+     * @return void
      * @author WangMingShuai
      * @date 2019/12/25 9:28
-     * @return void
      */
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        log.info("{}","项目启动完成，将服务注册到 oauth2 server");
+        log.info("{}", "项目启动完成，将服务注册到 oauth2 server");
         OauthClientDetailsDTO clientDetailDTO = OauthClientDetailsDTO.builder()
                 .clientId(clientId)
                 .clientSecret(clientSecret)
                 .resourceIds(resourceIds)
                 .scope("app")
                 .authorizedGrantTypes("authorization_code,refresh_token,password")
-                .webServerRedirectUri("http://127.0.0.1:9000/api/auth/callback")
+                .webServerRedirectUri(address + "/callback")
                 .accessTokenValidity(86400)
                 .autoapprove("true")
                 .build();
